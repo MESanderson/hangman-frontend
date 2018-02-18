13cf3012d2b3f104bb7c01d6f003f3cdb45570a2
MESanderson <mikeesanderson@gmail.com>
2018-02-18 15:42:55 -0500

    -Added react-bootstrap -Started laying out general classes/functionalities in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import {Row, Col} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+
+class HangManImage extends Component{
+	render(){
+		return <h2>Here be Image</h2>
+	}
+}
+
+class HangManWordToGuess extends Component{
+	render(){
+		return <h2>Here be word to guess</h2>
+	}
+}
+
+class HangeManGuessedCorrect extends Component{
+	render(){
+		return (
+		<div>
+		  <p>Guessed Correct</p>
+		  </div>
+	  )
+  }
+}
+
+class HangeManGuessedInCorrect extends Component{
+	render(){
+		return (
+		<div>
+		  <p>Guessed InCorrect</p>
+		  </div>
+	  )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        <h3 className="App-intro">
           Here be the hanging man.
-        </p>
+        </h3>
+        <Row>
+          <Col md={12}>
+			<HangManImage/>
+			<HangManWordToGuess/>
+          </Col>
+        </Row>
       </div>
     );
   }
