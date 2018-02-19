dc4ee791e3d08216b787d2a22b63299dde4dafcb
MESanderson <mikeesanderson@gmail.com>
2018-02-19 15:19:23 -0500

    Did some udpating on front end. Got bootstrap cdn actually linked. Still needs a long way to go.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,139 @@
 import React, { Component } from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, ListGroup, 
+	ListGroupItem, Button, FormControl} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class HangManImage extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			body_grid: [['', 'head', ''],
+			['left_arm', 'body', 'right_arm'],
+			['left_leg', '', 'right_leg']]
+		}
+	}
 	render(){
-		return <h2>Here be Image</h2>
+		const vis=this.state.body_grid.map((grid_row, ix_row) =>{
+			
+			let grid_items = grid_row.map((grid_item,ix_item) =>{
+				
+				let text_val;
+				
+				if (this.props.hangmanparts.indexOf(grid_item) === -1){
+					
+					text_val='';
+				} else{
+					text_val=grid_item
+				}
+				
+				return <Col md={4} key={ix_row+ix_item}><p>{text_val}</p></Col>
+			});
+			
+			return (
+			<Row key={ix_row}>
+				{grid_items}
+			</Row>
+			)
+		});
+		
+		return (
+		<Row>
+			<Col md={12}>
+				{vis}
+			</Col>
+		</Row>
+		)
 	}
 }
 
 class HangManWordToGuess extends Component{
 	render(){
-		return <h2>Here be word to guess</h2>
+		const hangman_pts = this.props.word.split('').map((letter,ix) =>{
+			let letter_val;
+			let className;
+			let letter_val_ix = this.props.correctLetters.indexOf(letter);
+			if (letter_val_ix === -1){
+				letter_val = '_';
+			} else {
+				letter_val = letter;
+				className='correct-letter';
+			}
+			return (
+			<Button key={'letter'+ix}>
+			{letter_val}
+			</Button>
+			)
+		});
+		return (
+		<Row className={"center"}>
+		<Col md={12}>
+		{hangman_pts}
+		</Col>
+		</Row>
+		)
 	}
 }
 
-class HangeManGuessedCorrect extends Component{
+class HangManGuessedCorrect extends Component{
 	render(){
+		const correct = this.props.correctLetters.map((letter,ix)=>{
+			return (
+			<ListGroupItem key={'correct'+ix}>
+			<p>{letter}</p>
+			</ListGroupItem>)
+		});
+		return (
+		<Row>
+		<Col md={12}>
+		  <p>Guessed Correct</p>
+		  <ListGroup>
+			{correct}
+			</ListGroup>
+			</Col>
+		  </Row>
+	  )
+  }
+}
+
+class HangManGuessedInCorrect extends Component{
+	render(){
+		const incorrect = this.props.incorrectLetters.map((letter,ix)=>{
+			return <ListGroupItem key={'incorrect'+ix}><p>{letter}</p></ListGroupItem>
+		});
 		return (
 		<div>
-		  <p>Guessed Correct</p>
+		  <p>Guessed InCorrect</p>
+		  <ListGroup>
+			{incorrect}
+			</ListGroup>
 		  </div>
 	  )
   }
 }
 
-class HangeManGuessedInCorrect extends Component{
+class HangManController extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+			word: 'serendipitous',
+			correctLetters: ['i'],
+			incorrectLetters: ['z'],
+			hangmanparts: ['head', 'left_arm', 'right_arm']
+		}
+	}
 	render(){
 		return (
-		<div>
-		  <p>Guessed InCorrect</p>
-		  </div>
-	  )
-  }
+		<Row>
+          <Col md={12}>
+			<HangManImage hangmanparts={this.state.hangmanparts}/>
+			<HangManWordToGuess word={this.state.word} correctLetters={this.state.correctLetters}/>
+			<HangManGuessedCorrect correctLetters={this.state.correctLetters}/>
+			<HangManGuessedInCorrect incorrectLetters={this.state.incorrectLetters}/>
+          </Col>
+        </Row>
+		)
+	}
 }
 
 
@@ -43,12 +144,7 @@ class App extends Component {
         <h3 className="App-intro">
           Here be the hanging man.
         </h3>
-        <Row>
-          <Col md={12}>
-			<HangManImage/>
-			<HangManWordToGuess/>
-          </Col>
-        </Row>
+        <HangManController/>
       </div>
     );
   }
