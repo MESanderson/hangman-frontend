6c34eb0b542de0f8172b35fd2abcdcc2e4b2b6f5
MESanderson <mikeesanderson@gmail.com>
2018-02-20 19:09:34 -0500

    finally have a very fragile, but working frontend

--- src/App.js
+++ src/App.js
@@ -3,44 +3,35 @@ import {Row, Col, ListGroup,
 	ListGroupItem, Button, FormControl} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import hangman_0 from './images/hangman_0.png';
+import hangman_1 from './images/hangman_1.png';
+import hangman_2 from './images/hangman_2.png';
+import hangman_3 from './images/hangman_3.png';
+import hangman_4 from './images/hangman_4.png';
+import hangman_5 from './images/hangman_5.png';
+import hangman_6 from './images/hangman_6.png';
 
 class HangManImage extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			body_grid: [['', 'head', ''],
-			['left_arm', 'body', 'right_arm'],
-			['left_leg', '', 'right_leg']]
+			hangman_images: [
+			hangman_0,
+			hangman_1,
+			hangman_2,
+			hangman_3,
+			hangman_4,
+			hangman_5,
+			hangman_6
+			]
 		}
 	}
 	render(){
-		const vis=this.state.body_grid.map((grid_row, ix_row) =>{
-			
-			let grid_items = grid_row.map((grid_item,ix_item) =>{
-				
-				let text_val;
-				
-				if (this.props.hangmanparts.indexOf(grid_item) === -1){
-					
-					text_val='';
-				} else{
-					text_val=grid_item
-				}
-				
-				return <Col md={4} key={ix_row+ix_item}><p>{text_val}</p></Col>
-			});
-			
-			return (
-			<Row key={ix_row}>
-				{grid_items}
-			</Row>
-			)
-		});
 		
 		return (
 		<Row>
 			<Col md={12}>
-				{vis}
+				<img src={this.state.hangman_images[this.props.num_incorrect_letters]}/>
 			</Col>
 		</Row>
 		)
@@ -68,6 +59,7 @@ class HangManWordToGuess extends Component{
 		return (
 		<Row className={"center"}>
 		<Col md={12}>
+		<h2>Guess the word</h2>
 		{hangman_pts}
 		</Col>
 		</Row>
@@ -79,9 +71,8 @@ class HangManGuessedCorrect extends Component{
 	render(){
 		const correct = this.props.correctLetters.map((letter,ix)=>{
 			return (
-			<ListGroupItem key={'correct'+ix}>
-			<p>{letter}</p>
-			</ListGroupItem>)
+			<Button key={'correct'+ix} >
+			{letter}</Button>)
 		});
 		return (
 		<Row>
@@ -96,17 +87,68 @@ class HangManGuessedCorrect extends Component{
   }
 }
 
+class GuessPanel extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			letter: ''
+		}
+		this.setLetterState = this.setLetterState.bind(this);
+		this.submitLetter = this.submitLetter.bind(this);
+	}
+	
+	setLetterState(e){
+		let letter = e.target.value;
+		if (letter !== ''){
+			this.setState(
+			{letter: letter}
+			);
+		}
+	}
+	
+	submitLetter(){
+		if (this.state.letter !== ''){
+			this.props.guess(this.state.letter);
+		}
+	}
+	
+	
+	render(){
+		const your_turn = true;
+		
+		if(your_turn){
+			return (
+				<Row>
+					<Col md={4}>
+						<Button bsStyle={'primary'} onClick={this.submitLetter}>Submit Guess</Button>
+					</Col>
+					<Col md={8}>
+						<FormControl onChange={this.setLetterState}/>
+					</Col>
+				</Row>
+				)
+		}else{
+			return (
+				<Row>
+					<Col md={12}>
+					<h3>Wait your turn</h3>
+					</Col>
+				</Row>
+			)
+		}
+		
+	}
+}
+
 class HangManGuessedInCorrect extends Component{
 	render(){
 		const incorrect = this.props.incorrectLetters.map((letter,ix)=>{
-			return <ListGroupItem key={'incorrect'+ix}><p>{letter}</p></ListGroupItem>
+			return <Button key={'incorrect'+ix}>{letter}</Button>
 		});
 		return (
 		<div>
-		  <p>Guessed InCorrect</p>
-		  <ListGroup>
+		  <p>Guessed Incorrect</p>
 			{incorrect}
-			</ListGroup>
 		  </div>
 	  )
   }
@@ -117,11 +159,24 @@ class HangManController extends Component{
 		super(props);
 		this.state={
 			word: 'serendipitous',
-			correctLetters: ['i'],
-			incorrectLetters: ['z'],
-			hangmanparts: ['head', 'left_arm', 'right_arm']
+			correctLetters: [],
+			incorrectLetters: []
+		}
+		this.guess = this.guess.bind(this);
+	}
+	
+	guess(letter){
+		if (this.state.word.indexOf(letter) !== -1){
+			this.setState(
+			{correctLetters: [].concat(this.state.correctLetters, [letter])}
+			);
+		} else{
+			this.setState(
+			{incorrectLetters: [].concat(this.state.incorrectLetters, [letter])}
+			)
 		}
 	}
+	
 	render(){
 		const classname={border: 'solid'};
 		return (
@@ -129,7 +184,8 @@ class HangManController extends Component{
           <Col md={12}>
 			<Row>
 				<Col md={4} className={classname}>
-					<HangManImage hangmanparts={this.state.hangmanparts}/>
+					<HangManImage hangmanparts={this.state.hangmanparts} 
+					num_incorrect_letters={this.state.incorrectLetters.length}/>
 				</Col>
 				<Col md={8}>
 					<Row>
@@ -145,6 +201,7 @@ class HangManController extends Component{
 							<HangManGuessedInCorrect incorrectLetters={this.state.incorrectLetters}/>
 						</Col>
 					</Row>
+					<GuessPanel guess={this.guess}/>
 				</Col>
 			</Row>
           </Col>
